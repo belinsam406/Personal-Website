27549e115b604cd00618cd47c590f6d24ab5f167
Sam <sam.belin@du.edu>
2023-06-08 16:01:26 -0600

    Got snow to work

--- thing.js
+++ thing.js
@@ -1,47 +1,52 @@
 const options = {
-    background: {
-      color: "#000", // the canvas background color
-    },
-    interactivity: {
-      events: {
-        onClick: {
-          // this handles the mouse click event
-          enable: true,
-          mode: "push", // this adds particles
-        },
-        onHover: {
-          // this handles the mouse hover event
-          enable: true,
-          mode: "repulse", // this make particles move away from the mouse
-        },
-      },
-      modes: {
-        push: {
-          quantity: 6, // number of particles to add
-        },
-        repulse: {
-          distance: 100, // the distance of the particles from the mouse
-        },
-      },
-    },
     particles: {
-      links: {
-        enable: true, // this enables links between particles
-        opacity: 0.3,
-        distance: 200,
+        number: {
+          value: 120,
+          density: {
+            enable: true,
+            area: 800
+          }
+        },
+        color: {
+          value: ["rgb(80,80,80)"]
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: 4,
+          random: true,
+        },
+        move: {
+          enable: true,
+          speed: .8,
+          direction: "bottom",
+          random: true,
+          straight: false,
+          outMode: "out",
+          bounce: true,
+        },
       },
-      move: {
-        enable: true, // this makes particles move
-        speed: { min: 1, max: 3 }, // this is the speed of the particles
+      "interactivity": {
+          "detectsOn": "window",
+          "events": {
+            "onClick": {
+              "enable": true,
+              "mode": "repulse",
+            },
+          },
+          "modes":{
+              "connect": {
+                  "distance": 1,
+                  "links": {
+                    "opacity": 0.1
+                  },
+                  "radius": 0
+                },
+          }
       },
-      opacity: {
-        value: { min: 0.3, max: 0.7 }, // this sets the opacity of the particles
-      },
-      size: {
-        value: { min: 1, max: 3 }, // this sets the size of the particles
-      },
-    },
-  };
+        
+    };
 
-  // tsParticles.load has two parameters, the first one is the id of the container, the second one is an object with the options
-  tsParticles.load("tsparticles", options);
+      // tsParticles.load has two parameters, the first one is the id of the container, the second one is an object with the options
+      tsParticles.load("tsparticles", options);
