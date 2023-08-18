dfde749d4390df851ef32bec52407c403b893d68
Sam Belin <sam.belin@du.edu>
2023-08-18 15:46:50 -0600

    finished password generator

--- computer_section/Random Password/random_password.js
+++ computer_section/Random Password/random_password.js
@@ -1,29 +1,27 @@
+
 function generateRandomPassword() { //, includeLower, includeUpper, includeNums, includeSpec
   length = parseInt(document.getElementById("length").value, 10);
   const includeSpecial = document.getElementById("includeSpecial").checked;
-
-  let charset = "";
-  let testset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+  const includeUpper = document.getElementById("includeUpper").checked;
+  const includeNums = document.getElementById("includeNums").checked;
+  let charset = "abcdefghijklmnopqrstuvwxyz";
+  // let testset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
   const charsetLower = "abcdefghijklmnopqrstuvwxyz";
   const charsetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const charsetNums = "0123456789";
   const charsetSpec = "!@#$%^&*()_+";
 
   // if(includeLower) charset += charsetLower;
-  // if(includeUpper) charset += charsetUpper;
-  // if(includeNums) charset += charsetNums
-  // if(includeSpec) charset += charsetSpec;
+  if(includeUpper) charset += charsetUpper;
+  if(includeNums) charset += charsetNums
+  if(includeSpecial) charset += charsetSpec;
 
   let password = "";
 
   for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * testset.length);
-    password += testset.charAt(randomIndex);
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
   }
   console.log(password);
   document.getElementById("password").textContent = "Generated Password: " + password;
 }
-
-  // Generate a random password with a length of 12 characters
-  // const randomPassword = generateRandomPassword(inputlength, includeLower, includeUpper, includeNums, includeSpec);
-  // console.log(randomPassword);
