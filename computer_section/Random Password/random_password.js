5df6f71934052b3b929627fb895a6e7ef689d180
Sam Belin <sam.belin@du.edu>
2023-08-18 15:36:19 -0600

    working on the password generator

--- computer_section/Random Password/random_password.js
+++ computer_section/Random Password/random_password.js
@@ -1,30 +1,29 @@
-let inputlength = 20;
-let includeLower = true;
-let includeUpper = false;
-let includeNums = false;
-let includeSpec = false;
-function generateRandomPassword(length, includeLower, includeUpper, includeNums, includeSpec) {
-    let charset = "";
-    const charsetLower = "abcdefghijklmnopqrstuvwxyz";
-    const charsetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const charsetNums = "0123456789";
-    const charsetSpec = "!@#$%^&*()_+";
+function generateRandomPassword() { //, includeLower, includeUpper, includeNums, includeSpec
+  length = parseInt(document.getElementById("length").value, 10);
+  const includeSpecial = document.getElementById("includeSpecial").checked;
 
-    if(includeLower) charset += charsetLower;
-    if(includeUpper) charset += charsetUpper;
-    if(includeNums) charset += charsetNums
-    if(includeSpec) charset += charsetSpec;
+  let charset = "";
+  let testset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+  const charsetLower = "abcdefghijklmnopqrstuvwxyz";
+  const charsetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const charsetNums = "0123456789";
+  const charsetSpec = "!@#$%^&*()_+";
 
-    let password = "";
-  
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * charset.length);
-      password += charset.charAt(randomIndex);
-    }
-  
-    return password;
+  // if(includeLower) charset += charsetLower;
+  // if(includeUpper) charset += charsetUpper;
+  // if(includeNums) charset += charsetNums
+  // if(includeSpec) charset += charsetSpec;
+
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * testset.length);
+    password += testset.charAt(randomIndex);
   }
-  
+  console.log(password);
+  document.getElementById("password").textContent = "Generated Password: " + password;
+}
+
   // Generate a random password with a length of 12 characters
-  const randomPassword = generateRandomPassword(inputlength, includeLower, includeUpper, includeNums, includeSpec);
-  console.log(randomPassword);
+  // const randomPassword = generateRandomPassword(inputlength, includeLower, includeUpper, includeNums, includeSpec);
+  // console.log(randomPassword);
