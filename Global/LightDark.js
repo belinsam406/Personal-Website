1e504b34df04075f19a104b620072e8abc536acf
Sam Belin <belinsam406@gmail.com>
2023-11-24 17:18:23 -0500

    working on light and dark themes

---
+++ Global/LightDark.js
@@ -0,0 +1,11 @@
+//this is where i will put the code for light and dark mode switching
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+    const currentTheme = body.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+    body.setAttribute('data-theme', newTheme);
+});
+
