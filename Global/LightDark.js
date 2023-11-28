32d9b88e9f49361baa7c69d3bdad4a00fa530bf9
Sam Belin <belinsam406@gmail.com>
2023-11-27 21:16:55 -0700

    adding light and dark slider

--- Global/LightDark.js
+++ Global/LightDark.js
@@ -1,3 +1,4 @@
+//written with help from https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
 function switchTheme(e) {
@@ -16,14 +17,14 @@ function switchTheme(e) {
 toggleSwitch.addEventListener('change', switchTheme, false);
 
 //storing the decision for future visits
-// function switchTheme(e) {
-//     if (e.target.checked) {
-//         document.documentElement.setAttribute('data-theme', 'dark');
-//         localStorage.setItem('theme', 'dark'); //add this
-//     }
-//     else {
-//         document.documentElement.setAttribute('data-theme', 'light');
-//         localStorage.setItem('theme', 'light'); //add this
-//     }
-// }
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }
+}
 
