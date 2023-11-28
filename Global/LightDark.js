8b80e17ca942e37c7b6fbe1e79024186f7f9b95e
Sam Belin <belinsam406@gmail.com>
2023-11-27 20:07:29 -0700

    working on light and dark mode cant get particles to coperate

--- Global/LightDark.js
+++ Global/LightDark.js
@@ -1,11 +1,31 @@
 //this is where i will put the code for light and dark mode switching
-const themeToggle = document.getElementById('theme-toggle');
-const body = document.body;
 
-themeToggle.addEventListener('click', () => {
-    const currentTheme = body.getAttribute('data-theme');
-    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
-    body.setAttribute('data-theme', newTheme);
-});
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        console.log("dark theme");
+        // tsParticles.load("tsparticles", darkTheme);
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        console.log("light theme");
+        // tsParticles.load("tsparticles", lightTheme);
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+//storing the decision for future visits
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }    
+}
 
--- snow.js
+++ snow.js
@@ -1,9 +1,9 @@
-const options = {
-    name: "Snow",
+const lightTheme = {
+    name: "SnowLight",
     fpsLimit: 240,
     particles: {
         number: {
-            value: 50,
+            value: 100,
             density: {
                 enable: true,
             },
@@ -12,7 +12,7 @@ const options = {
             value: "#FFFFFF",
         },
         shape: {
-            type: "square"
+            type: "circle"
         },
         opacity: {
             value: 1,
@@ -48,27 +48,72 @@ const options = {
     },
     background: {
         color: {
-            value: "transparent",
+            value: "#89cfeb",
           },
-    },
-    emitters: {
-        position: {
-            y: 0,
-            x: 67
-        },
-        rate: {
-            delay: 1,
-            quantity: 1
-        },
-        size: {
-            width: 1,
-            height: 1
-        },
     },
 };
 
+const darkTheme={
+    name: "SnowDark",
+    fpsLimit: 240,
+    particles: {
+        number: {
+            value: 100,
+            density: {
+                enable: true,
+            },
+        },
+        color: {
+            value: "#FFFFFF",
+        },
+        shape: {
+            type: "circle"
+        },
+        opacity: {
+            value: 1,
+        },
+        size: {
+            value: 2,
+        },
+        move: {
+            angle: {
+                offset: 0,
+                value: 100,
+            },
+            enable: true,
+            speed: 2,
+            direction: "bottom",
+            straight: false,
+            wobble: true,
+        },
+        wobble: {
+            enable: true,
+            distance: 5,
+            speed: 1,
+        },
+        zIndex: {
+            value: {
+                min: 0,
+                max: 100,
+            },
+            opacityRate: 10,
+            sizeRate: 10,
+            velocityRate: 10,
+        },
+    },
+    background: {
+        color: {
+            value: "#145a76",
+          },
+    },
+};
+
+
+
 // tsParticles.load has two parameters, the first one is the id of the container, the second one is an object with the options
-tsParticles.load("tsparticles", options);
+tsParticles.load("tsparticles", lightTheme);
+// tsParticles.load("tsparticles", darkTheme);
+
 
 let tp = document.getElementById("tsparticles");
 window.addEventListener("scroll", (e) => {
