3258803a0ef5970d3cdb6cbfc1a792aee2771694
Sam Belin <belinsam406@gmail.com>
2023-12-04 15:13:22 -0700

    combining some files and fixing the background so the snow does not reset when changing from light to dark

--- Global/snow.js
+++ Global/snow.js
@@ -1,4 +1,7 @@
-const lightTheme = {
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+const backgroundTheme = {
     name: "SnowLight",
     fpsLimit: 240,
     particles: {
@@ -48,62 +51,7 @@ const lightTheme = {
     },
     background: {
         color: {
-            value: "#89cfeb",
-          },
-    },
-};
-
-const darkTheme={
-    name: "SnowDark",
-    fpsLimit: 240,
-    particles: {
-        number: {
-            value: 100,
-            density: {
-                enable: true,
-            },
-        },
-        color: {
-            value: "#FFFFFF",
-        },
-        shape: {
-            type: "circle"
-        },
-        opacity: {
-            value: 1,
-        },
-        size: {
-            value: 2,
-        },
-        move: {
-            angle: {
-                offset: 0,
-                value: 100,
-            },
-            enable: true,
-            speed: 2,
-            direction: "bottom",
-            straight: false,
-            wobble: true,
-        },
-        wobble: {
-            enable: true,
-            distance: 5,
-            speed: 1,
-        },
-        zIndex: {
-            value: {
-                min: 0,
-                max: 100,
-            },
-            opacityRate: 10,
-            sizeRate: 10,
-            velocityRate: 10,
-        },
-    },
-    background: {
-        color: {
-            value: "#145a76",
+            value: "transparent",
           },
     },
 };
@@ -111,8 +59,30 @@ const darkTheme={
 
 
 // tsParticles.load has two parameters, the first one is the id of the container, the second one is an object with the options
-tsParticles.load("tsparticles", lightTheme);
+// lightTheme.background.color = "#89cfeb";
+tsParticles.load("tsparticles", backgroundTheme);
 
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        localStorage.setItem('theme', 'dark');
+        document.documentElement.setAttribute('data-theme', 'dark');
+
+    }
+    else {
+        localStorage.setItem('theme', 'light');
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
 
 let tp = document.getElementById("tsparticles");
 window.addEventListener("scroll", (e) => {
