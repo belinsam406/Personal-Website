83c5a99aac1b0674aa8377af7d8e8e7d74c86b39
Sam Belin <belinsam406@gmail.com>
2024-04-11 20:13:00 -0600

    working on neral net stuff

---
+++ computer_section/Neral_net_practice/JS/primitives/point.js
@@ -0,0 +1,26 @@
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    equals(point) {
+        return this.x == point.x && this.y == point.y;
+    }
+
+    draw(ctx, { size = 18, color = "black", outline } = {}) {
+        const rad = size / 2;
+        ctx.beginPath();
+        ctx.fillStyle = color;
+        ctx.arc(this.x, this.y, rad, 0, Math.PI * 2);
+        ctx.fill();
+        if (outline) {
+            ctx.beginPath();
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "white";
+            ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2);
+            ctx.stroke();
+            ctx.strokeStyle = "black";
+        }
+    }
+}
