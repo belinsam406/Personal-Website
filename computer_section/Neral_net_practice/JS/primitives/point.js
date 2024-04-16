f2054d801529224340f32ef45e3017cff2054ca8
Sam Belin <belinsam406@gmail.com>
2024-04-16 13:07:49 -0600

    adding trees and buldings

--- computer_section/Neral_net_practice/JS/primitives/point.js
+++ computer_section/Neral_net_practice/JS/primitives/point.js
@@ -8,7 +8,7 @@ class Point {
         return this.x == point.x && this.y == point.y;
     }
 
-    draw(ctx, { size = 18, color = "black", outline, fill = false } = {}) {
+    draw(ctx, { size = 18, color = "black", outline = false, fill = false } = {}) {
         const rad = size / 2;
         ctx.beginPath();
         ctx.fillStyle = color;
@@ -17,15 +17,14 @@ class Point {
         if (outline) {
             ctx.beginPath();
             ctx.lineWidth = 2;
-            ctx.strokeStyle = "white";
+            ctx.strokeStyle = "yellow";
             ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2);
             ctx.stroke();
         }
-        if(fill){
+        if (fill) {
             ctx.beginPath();
-            ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2);
-            ctx.stroke();
-            ctx.fillStyle = "blue";
+            ctx.arc(this.x, this.y, rad * 0.4, 0, Math.PI * 2);
+            ctx.fillStyle = "yellow";
             ctx.fill();
         }
     }
